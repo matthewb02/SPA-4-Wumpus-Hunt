520f971f6c11954b82dd720f7018f51cae5f7b66
benderm <benderm@msoe.edu>
2025-03-28 11:58:15 -0500

    removed todo

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,6 @@
 
 using namespace std;
 
-/*
- * @TODO
- * Game class - takes game logic out of the driver and allows "global variables"
- *
- * stairs - descend between floors that extend in size
- *
- * arrow functionality - option to shoot adjacent rooms
- *
- * special rooms - campfire, locked door, blessed fountain
- */
-
 int main() {
     Game game = Game();
     game.run();
